74ca7620b4313525203a55a12270d0b5a621b999
Plamen Valentinov <pvkdeveloper@gmx.com>
2019-01-14 17:49:40 +0100

    Cli basic tests finished.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,17 +8,15 @@ from steganogan import cli
 from steganogan.models import SteganoGAN
 
 @patch('steganogan.cli.SteganoGAN.load')
-def test__get_steganogan(stega_load_mock):
+def test__get_steganogan(mock_steganogan_load):
     """
-    VALIDATE:
+    Test that:
         * The model path is the right one, with the right architecture
         * SteganoGAN.load is called with the right values
         * The output of SteganoGAN.load is returned
-
-    MOCK:
-        * SteganoGAN
-        * args
     """
+
+    mock_steganogan_load.return_value = 'Steganogan'
 
     # setup
     params = MagicMock()
@@ -36,47 +34,41 @@ def test__get_steganogan(stega_load_mock):
     cli_test = cli._get_steganogan(params)
 
     # assert
-    expected_stega_call = call(model_path, cuda=not params.cpu, verbose=params.verbose)
+    mock_steganogan_load.assert_called_once_with(
+        model_path,
+        cuda=False,
+        verbose=True
+    )
 
-    assert expected_stega_call == stega_load_mock.call_args_list
+    assert cli_test == 'Steganogan'
 
 
-@patch('steganogan.cli.SteganoGAN')
-def test__encode(mock_steganogan):
-    """Test that encode has been called with the proper args"""
-    """
-    VALIDATE:
-        * steganogan.encode has been called with the right values
-
-    MOCK:
-        * cli._get_steganogan to return a mock of steganogan
-    """
+@patch('steganogan.cli._get_steganogan')
+def test__encode(mock__get_steganogan):
+    """Test that encode has been called with the args that the cli recived."""
+    steganogan = MagicMock()
+    mock__get_steganogan.return_value = steganogan
     # setup
     params = MagicMock()
-    type(params).cpu = PropertyMock(return_value=True)
-    type(params).architecture = PropertyMock(return_value='basic')
-    type(params).verbose = PropertyMock(return_value=True)
+    params.cpu = True
+    params.architecture = 'basic'
+    params.verbose = True
 
-    type(params).cover = PropertyMock(return_value='image.jpg')
-    type(params).message = PropertyMock(return_value='Hello world')
-    type(params).output = PropertyMock(return_value='output.png')
-
-    model_name = '{}.steg'.format(params.architecture)
-    parent_path = os.path.dirname(os.path.dirname(__file__))
-    stega_path = os.path.join(parent_path, 'steganogan')
-    pretrained_path = os.path.join(stega_path, 'pretrained')
-    model_path = os.path.join(pretrained_path, model_name)
+    params.cover = 'image.jpg'
+    params.message = 'Hello world'
+    params.output = 'output.png'
 
     # run
-    with patch('steganogan.cli._get_steganogan') as mock_get_steganogan:
-        mock_get_steganogan.return_value = mock_steganogan
-        cli._encode(params)
+    cli._encode(params)
 
     # assert
+    mock__get_steganogan.assert_called_once_with(params)
+    steganogan.encode.assert_called_once_with('image.jpg', 'output.png', 'Hello world')
 
 
 
-def test__decode():
+@patch('steganogan.cli._get_steganogan')
+def test__decode(mock__get_steganogan):
     """
     VALIDATE:
         * steganogan.decode has been called with the right values
@@ -84,30 +76,22 @@ def test__decode():
     MOCK:
         * mock cli._get_steganogan to return a mock of steganogan
     """
-    pass
+    # setup
+    steganogan = MagicMock()
+    steganogan.decode.return_value = 'Hello world'
 
+    mock__get_steganogan.return_value = steganogan
 
-# @patch('steganogan.cli._get_steganogan')
-# def test__encode_image(get_stega_mock):
-#     """Given a real image, test that it encodes it with the basic.steg in fixtures."""
-#     # setup
-#     this_dir = os.path.dirname(__file__)
-#     img = os.path.join(this_dir, 'fixtures/images/pythia.jpg')
-#     output = os.path.join(this_dir, 'fixtures/images/out.png')
-#     steg_path = os.path.join(this_dir, 'fixtures/pretrained/basic.steg')
-# 
-#     steganogan = SteganoGAN.load(steg_path, cuda=False, verbose=False)
-# 
-#     get_stega_mock.return_value = steganogan
-# 
-#     args = MagicMock()
-#     type(args).cover = PropertyMock(return_value=img)
-#     type(args).output = PropertyMock(return_value=output)
-#     type(args).message = PropertyMock(return_value='Hello world')
-# 
-#     # run
-#     cli._encode(args)
-# 
-#     # assert
-# 
-#     assert 'Hello world' == steganogan.decode(output)
+    params = MagicMock()
+    params.cpu = True
+    params.architecture = 'basic'
+    params.verbose = True
+
+    params.image = 'test_image.png'
+
+    # run
+    cli._decode(params)
+
+    # assert
+    mock__get_steganogan.assert_called_once_with(params)
+    steganogan.decode.assert_called_once_with('test_image.png')
