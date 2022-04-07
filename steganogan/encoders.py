0d4ef9d47e6f7c2f0ebac00039f55653681224ff
soltanianaref <101413285+soltanianaref@users.noreply.github.com>
2022-04-07 07:43:41 -0700

    Update encoders.py

--- steganogan/encoders.py
+++ steganogan/encoders.py
@@ -2,6 +2,12 @@
 
 import torch
 from torch import nn
+import torch.onnx
+from torchvision.ops.deform_conv import DeformConv2d
+
+input1 = torch.rand(4, 3, 10, 10)
+kh, kw = 3, 3
+offset1 = torch.rand(4, 2 * kh * kw, 8, 8)
 
 
 class BasicEncoder(nn.Module):
@@ -15,12 +21,10 @@ class BasicEncoder(nn.Module):
 
     add_image = False
 
-    def _conv2d(self, in_channels, out_channels):
-        return nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=3,
-            padding=1
+    def _conv2d(self, input1, offset1):
+        return DeformConv2d(
+            input1=input1,
+            offset1=offset1,
         )
 
     def _build_models(self):
